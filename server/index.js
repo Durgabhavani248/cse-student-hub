2f450849a7d61c1fc597b5b8d3216f3c9591d2e9
Durgabhavani248 <adurgabhavani100@gmail.com>
2026-07-05 15:50:56 +0530

    fix firebase admin import for render

--- server/index.js
+++ server/index.js
@@ -19,13 +19,13 @@ if (!fs.existsSync("uploads")) {
 
 dotenv.config();
 
-import admin from "firebase-admin";
+import * as admin from "firebase-admin";
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   }),
 });
 
